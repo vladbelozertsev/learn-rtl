07c242199998c4627499a56ae54cd99600eb00df
someuserABC <someuserABC@gmail.com>
2021-04-19 15:01:15 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
+const Search = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <label htmlFor="search">{props.children}</label>
+      <input type="text" id="search" defaultValue={props.value} onChange={props.onChange} />
     </div>
   );
-}
+};
+
+const App = () => {
+  const [search, setSearch] = useState('');
+
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
+
+  useEffect(() => {
+    console.log(search);
+  }, [search]);
+
+  return (
+    <div>
+      <Search value={search} onChange={handleChange}>
+        Search:
+      </Search>
+      <p>Searches for {search ? search : '...'}</p>
+    </div>
+  );
+};
 
 export default App;
